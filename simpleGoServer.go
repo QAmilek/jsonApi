c08bcc65b052d11771cb45841fe6045e4c354fc2
kamil <kamil.chwedyna@gmail.com>
2016-05-17 00:44:40 +0200

    More complex server: articles

--- simpleGoServer.go
+++ simpleGoServer.go
@@ -1,18 +1,45 @@
 package main
 
 import (
-	"fmt"
-	"html"
-	"log"
-	"net/http"
+    "fmt"
+    "log"
+    "net/http"
 )
 
-func main() {
-	http.HandleFunc("/", printUrl)
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
+func homePage(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintf(w, "Welcome to the HomePage!")
+    fmt.Println("Endpoint Hit: homePage")
 }
 
-func printUrl (w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+func returnArticle(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "returns a specific article")
+    fmt.Println("Endpoint Hit: returnArticle")
+}
+
+func returnAllArticles(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintf(w, "All Articles")
+    fmt.Println("Endpoint Hit: returnAllArticles")
+}
+
+func addArticle(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintf(w, "Adds an article to list of articles")
+    fmt.Println("Endpoint Hit: addArticle")
+}
+
+func delArticle(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintf(w, "deletes a specific article")
+    fmt.Println("Endpoint Hit: delArticle")
+}
+
+func handleRequests() {
+    http.HandleFunc("/", homePage)
+    http.HandleFunc("/all", returnAllArticles)
+    http.HandleFunc("/single", returnArticle)
+    http.HandleFunc("/delete", delArticle)
+    http.HandleFunc("/add", addArticle)
+    log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func main() {
+    handleRequests()
 }
